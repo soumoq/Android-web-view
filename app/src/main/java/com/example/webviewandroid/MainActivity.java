809549df9ecba863d02fadd264ae8cf70f643d3e
soumojyotiganguly270@gmail.com <soumojyotiganguly270@gmail.com>
2020-05-26 15:06:56 +0530

    add web site and hide title ber

--- app/src/main/java/com/example/webviewandroid/MainActivity.java
+++ app/src/main/java/com/example/webviewandroid/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.webviewandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,8 +19,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        webView=(WebView)findViewById(R.id.web_view);
-        webView.setWebChromeClient(new WebChromeClient());
-        webView.loadUrl("https://www.google.com");
+        webView = (WebView) findViewById(R.id.web_view);
+        try {
+            Toast.makeText(this, "In web view", Toast.LENGTH_LONG).show();
+            WebSettings webSettings = webView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            webView.loadUrl("file:///android_asset/nuclear_project_short3/index.html");
+            webView.setWebChromeClient(new WebChromeClient());
+        } catch (Exception e) {
+            Toast.makeText(this, "Excperion: " + e, Toast.LENGTH_LONG).show();
+        }
+
     }
 }
